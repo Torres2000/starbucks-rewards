56808b3725ed82dca3b282ad119bfd3bceced577
Torres2000 <pedro21t25@gmail.com>
2024-06-07 16:27:18 -0500

    diseño de inicio listo

--- src/Components/Main/Inicio/index.tsx
+++ src/Components/Main/Inicio/index.tsx
@@ -1,8 +1,8 @@
 export default function Inicio() {
   return (
     <>
-      <section className="fondoInicio pb-10 h-[75vh] sm:h-screen md:h-[42vh] md:flex md:bg-bottom md:p-0 md:mb-10 lg:h-[89vh]">
-        <div className="flex flex-col items-center px-6 py-7 md:justify-center md:items-start md:max-w-[400px] lg:px-10 lg:min-w-[48%]">
+      <section className="fondoInicio pb-10 h-[76vh] sm:h-screen md:h-[42vh] md:flex md:bg-bottom md:p-0 md:mb-10 lg:h-[89vh] xl:h-[100vh] 2xl:h-[80vh]">
+        <div className="  flex flex-col items-center px-6 py-7 md:justify-center md:items-start md:max-w-[400px] lg:px-10 lg:min-w-[48%] 2xl:m-auto 2xl:w-full 2xl:max-w-[1440px] 2xl:p-0">
           <h2 className="text-[26px] font-bold text-center md:text-left md:text-[32px]">
             FREE COFFEE
             <br />
@@ -30,7 +30,7 @@ export default function Inicio() {
               <a href="#" className=" border-b-black border md:hidden">
                 Or join online
               </a>
-              <span className="md:text-lg">
+              <span className="hidden md:inline md:text-lg">
                 Or{" "}
                 <a href="#" className="hidden border-b-black border md:inline">
                   join in the app
