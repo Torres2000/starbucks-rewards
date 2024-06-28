729429b551712ac5c948b8b6f34eba08bf494c27
Torres2000 <pedro21t25@gmail.com>
2024-06-28 17:08:45 -0500

    Segunda parte de terminos

--- src/Components/Main/Terms-Conditions/page.tsx
+++ src/Components/Main/Terms-Conditions/page.tsx
@@ -5,7 +5,7 @@ export default function TermsConditions() {
     <>
       <section className="pt-6 xl:pt-7">
         <div className="max-w-[1440px] mx-auto">
-          <div className="px-4 xl:px-9 2xl:p-0">
+          <div className="px-4 xl:px-9 2xl:p-0 lg:px-9 ">
             <div>
               <h2 className="text-2xl font-semibold">Terms and Conditions</h2>
               <div>
@@ -20,7 +20,9 @@ export default function TermsConditions() {
                     be earned on purchases of alcohol, Starbucks Cards and
                     Starbucks Card reloads. For details, visit{" "}
                     <a href="#" className="inline-flex items-center">
-                      <span>deltastarbucks.com/terms </span>{" "}
+                      <span className="border-b-[#006241] text-[#006241] border-[1px] leading-none">
+                        deltastarbucks.com/terms{" "}
+                      </span>{" "}
                       <ArrowSquareOut size={14} />
                     </a>
                     .<a href="#">↩</a>
@@ -33,10 +35,14 @@ export default function TermsConditions() {
                     purchases of alcohol or on reloads of Starbucks Cards that
                     are not registered. For details, visit{" "}
                     <a href="#" className="inline-flex items-center">
-                      <span>Terms and Conditions</span>{" "}
+                      <span className="border-b-[#006241] text-[#006241] border-[1px] leading-none">
+                        Terms and Conditions
+                      </span>{" "}
                     </a>
                     Bank of America, N.A. Member FDIC.
-                    <a href="#">↩</a>
+                    <a href="#" className="inline-flex items-center">
+                      ↩
+                    </a>
                   </p>
                   <p className="text-sm pb-3 2xl:text-base">
                     <sup>3</sup>Individuals must link loyalty program accounts
@@ -56,7 +62,9 @@ export default function TermsConditions() {
                     conditions. Other restrictions and exclusions apply. For
                     full offer terms and conditions, visit{" "}
                     <a href="#" className="inline-flex items-center">
-                      <span>Starbucks.com/MarriottBonvoy</span>{" "}
+                      <span className="border-b-[#006241] text-[#006241] border-[1px] leading-none">
+                        Starbucks.com/MarriottBonvoy
+                      </span>{" "}
                       <ArrowSquareOut size={14} />
                     </a>
                     . Marriott Bonvoy® and its affiliated brands and hotels are
@@ -65,11 +73,80 @@ export default function TermsConditions() {
                     from Marriott International, Inc.<a href="#">↩</a>
                   </p>
                 </div>
-                <div>
-                  <div></div>
-                  <div></div>
-                  <div></div>
-                  <div></div>
+                <div className="grid grid-cols-2 gap-x-6">
+                  <div className="pb-5">
+                    <h3 className="font uppercase text-[#00000094] font-bold text-sm pb-4">
+                      Earning Stars
+                    </h3>
+                    <p className="text-sm pb-3">
+                      Stars cannot be earned on purchases of alcohol, Starbucks
+                      Cards or Starbucks Card reloads.
+                    </p>
+                    <p className="text-sm pb-3">
+                      Earn 1 Star per $1 spent when you scan your member barcode
+                      in the app, then pay with cash, credit/debit cards or
+                      mobile wallets at participating stores. You can also earn
+                      1 Star per $1 spent when you link a payment method and pay
+                      directly through the app.
+                    </p>
+                    <p className="text-sm pb-3">
+                      Earn 2 Stars per $1 spent when you load funds and pay with
+                      your digital Starbucks Card in the app. You can also earn
+                      2 Stars per $1 spent when you pay in-person at a
+                      participating store with your registered physical
+                      Starbucks Card or scan your member barcode in the app, and
+                      then use any physical Starbucks Card (regardless of
+                      whether it is registered) to complete the purchase.
+                    </p>
+                  </div>
+                  <div>
+                    <h3 className="pb-3 uppercase text-[#00000094] font-bold">
+                      Terms of Use
+                    </h3>
+                    <p className="text-sm pb-3">
+                      For full program details visit{" "}
+                      <a href="#" className="inline-flex items-center">
+                        <span className="border-b-[#006241] text-[#006241] border-[1px] leading-none">
+                          starbucks.com/rewards/terms
+                        </span>{" "}
+                        <ArrowSquareOut />.
+                      </a>
+                    </p>
+                    <p className="text-sm pb-3">
+                      Starbucks® Rewards benefits are available at participating
+                      Starbucks stores. Not all stores have the ability to honor
+                      Rewards at this time. Visit the{" "}
+                      <a href="#" className="inline-flex items-center">
+                        <span className="border-b-[#006241] text-[#006241] border-[1px] leading-none">
+                          Starbucks Store Locator
+                        </span>
+                        <ArrowSquareOut />.
+                      </a>
+                      and search for locations honoring “Redeem Rewards”.
+                    </p>
+                  </div>
+                  <div className="pb-5">
+                    <h3 className="pb-3 uppercase text-[#00000094] font-bold">
+                      Benefits
+                    </h3>
+                    <p className="text-sm pb-3">
+                      Free refills available during same in-store visit only. To
+                      qualify for the Birthday Reward, you must have made at
+                      least one Star-earning transaction.
+                    </p>
+                  </div>
+                  <div className="pb-5">
+                    <h3 className="pb-3 uppercase text-[#00000094] font-bold">
+                      Redeeming Rewards
+                    </h3>
+                    <p className="text-sm pb-3">
+                      Rewards cannot be redeemed for alcoholic beverages or
+                      multi-serve items. You pay the difference for any beverage
+                      customization over $1 and/or merchandise item over $20.
+                      Not all stores honor tiered Rewards. Select stores redeem
+                      200 Stars for free food or drink items only.
+                    </p>
+                  </div>
                 </div>
               </div>
             </div>
