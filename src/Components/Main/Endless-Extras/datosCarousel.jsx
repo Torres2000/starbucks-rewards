c9681caadf0b2d897b6a8324dd3b599666c4c14a
Torres2000 <pedro21t25@gmail.com>
2024-07-04 13:07:07 -0500

    Cambio de nombre a la importacion

--- src/Components/Main/Endless-Extras/datosCarousel.jsx
+++ src/Components/Main/Endless-Extras/datosCarousel.jsx
@@ -1,66 +1,66 @@
-import A1 from '@/img/endless-extras/freebies/1A.webp'
-import A2 from '@/img/endless-extras/freebies/1B.webp'
-import A3 from '@/img/endless-extras/freebies/1C.webp'
-import B1 from '@/img/endless-extras/order/2A.webp'
-import B2 from '@/img/endless-extras/ORDER/2B.webp'
-import C1 from '@/img/endless-extras/faster/3A.webp'
-import C2 from '@/img/endless-extras/faster/3B.webp'
-import C3 from '@/img/endless-extras/faster/3C.webp'
+import A1 from "@/img/endless-extras/freebies/1A.webp";
+import A2 from "@/img/endless-extras/freebies/1B.webp";
+import A3 from "@/img/endless-extras/freebies/1C.webp";
+import B1 from "@/img/endless-extras/order/2A.webp";
+import B2 from "@/img/endless-extras/order/2B.webp";
+import C1 from "@/img/endless-extras/faster/3A.webp";
+import C2 from "@/img/endless-extras/faster/3B.webp";
+import C3 from "@/img/endless-extras/faster/3C.webp";
 const datos = {
   freebies: [
-{
-  img: A1,
-  title: "Free food, drinks & more",
-  descripcion: "Redeem your Stars for favorites like a handcrafted cold brew, protein box or even a coffee tumbler."
-  
-},
-{
-  img: A2,
-  title: "Complimentary birthday treat",
-  descripcion: "Every year on your birthday get a free drink or food item of your choice."
-  
-},
-{
-  img: A3,
-  title: "Free food, drinks & more",
-  descripcion: "More Caffé Verona® Blend, please. We’re happy to refill your hot cup while you’re in the store. Just ask."
-  
-}
-],
-order: [
-  {
-    img: B1,
-    title: "Tag, go.",
-    descripcion: "Select your menu items in the app along with your store location. Then tell the barista your name when you swing by the pickup area to grab your order."
-    
-  }, 
-  {
-    img: B2,
-    title: "Just for you",
-    descripcion: "Our app lets you customize your order and track an estimated pickup time. Make sure to save your favorites so they’re all ready for your next order."
-    
-  }, 
-],
-faster: [
-  {
-    img: C1,
-    title: "Bonus Star challenges",
-    descripcion: "Rack up the Stars with regular opportunities to get rewarded for what you love."
-    
-  }, 
-  {
-    img: C2,
-    title: "Double Star Days",
-    descripcion: "Watch for those special days where you’ll earn twice the Stars on almost everything."
-    
-  }, 
-  {
-    img: C3,
-    title: "Member-only games",
-    descripcion: "Play for a chance to win exclusive prizes, free food and drinks, and more."
-    
-  }, 
-]
-}
+    {
+      img: A1,
+      title: "Free food, drinks & more",
+      descripcion:
+        "Redeem your Stars for favorites like a handcrafted cold brew, protein box or even a coffee tumbler.",
+    },
+    {
+      img: A2,
+      title: "Complimentary birthday treat",
+      descripcion:
+        "Every year on your birthday get a free drink or food item of your choice.",
+    },
+    {
+      img: A3,
+      title: "Free food, drinks & more",
+      descripcion:
+        "More Caffé Verona® Blend, please. We’re happy to refill your hot cup while you’re in the store. Just ask.",
+    },
+  ],
+  order: [
+    {
+      img: B1,
+      title: "Tag, go.",
+      descripcion:
+        "Select your menu items in the app along with your store location. Then tell the barista your name when you swing by the pickup area to grab your order.",
+    },
+    {
+      img: B2,
+      title: "Just for you",
+      descripcion:
+        "Our app lets you customize your order and track an estimated pickup time. Make sure to save your favorites so they’re all ready for your next order.",
+    },
+  ],
+  faster: [
+    {
+      img: C1,
+      title: "Bonus Star challenges",
+      descripcion:
+        "Rack up the Stars with regular opportunities to get rewarded for what you love.",
+    },
+    {
+      img: C2,
+      title: "Double Star Days",
+      descripcion:
+        "Watch for those special days where you’ll earn twice the Stars on almost everything.",
+    },
+    {
+      img: C3,
+      title: "Member-only games",
+      descripcion:
+        "Play for a chance to win exclusive prizes, free food and drinks, and more.",
+    },
+  ],
+};
 
-export default datos
+export default datos;
