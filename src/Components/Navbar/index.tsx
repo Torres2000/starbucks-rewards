446871bf4ed4e728ac98966f021f28d00d4b9e98
Torres2000 <pedro21t25@gmail.com>
2024-07-04 09:55:15 -0500

    Agregando zindex al navbar en version movil

--- src/Components/Navbar/index.tsx
+++ src/Components/Navbar/index.tsx
@@ -58,7 +58,7 @@ export default function Navbar() {
             </div>
           </section>
           <section
-            className={`h-[calc(100%-64px)]  bottom-auto fixed z-10 w-full bg-black/35  sm:h-[calc(100%-83px)] top-[73px] block md:top-[83px] lg:bg-white lg:relative lg:top-0 lg:h-full ${
+            className={`h-[calc(100%-64px)]  bottom-auto fixed z-20 w-full bg-black/35  sm:h-[calc(100%-83px)] top-[73px] block md:top-[83px] lg:bg-white lg:relative lg:top-0 lg:h-full ${
               abierto ? "" : "hidden lg:block  "
             }`}
           >
